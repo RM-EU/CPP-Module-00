4680d4270757379b8cdefc35a542cbbd25cce26e
mracz <mracz@student.42wolfsburg.de>
2026-02-17 23:16:49 +0100

    building the constructor

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,9 +6,28 @@
 /*   By: mracz <mracz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 21:12:50 by mracz             #+#    #+#             */
-/*   Updated: 2026/02/16 21:14:37 by mracz            ###   ########.fr       */
+/*   Updated: 2026/02/17 23:15:53 by mracz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account( int initial_deposit )
+{
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_amount = initial_deposit;
+	_totalAmount += initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+}
+
+void	makeDeposit( int deposit )
+{
+	
+}
